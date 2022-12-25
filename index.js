3a1b9c0cce8e495b52ea0d9973d108c77f662f3d
Cihan Alcin <113132289+CihanNscc@users.noreply.github.com>
2022-12-24 22:56:19 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function paintShop() {
-  let gallonPerArea = 150.0;
+  let canPerArea = parseInt(document.getElementById("cansOfPaint").value);
   let roomLength = parseInt(document.getElementById("roomLength").value);
   let roomWidth = parseInt(document.getElementById("roomWidth").value);
   let roomHeight = parseInt(document.getElementById("roomHeight").value);
 
   let areaOutput = (roomLength * roomHeight * 2) + (roomWidth * roomHeight * 2);
-  let paintOutput = Math.ceil(areaOutput / gallonPerArea);
+  let paintOutput = Math.ceil(areaOutput / canPerArea);
 
   document.getElementById("areaOutput").innerHTML = areaOutput;
   document.getElementById("paintOutput").innerHTML = paintOutput;
